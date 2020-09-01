418daf48c892c07a6ba921eeb97e13231805d933
GitGangGuy <git@dothomic.de>
2020-09-02 01:21:05 +0200

    fix(sync.sh): small fix

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -2,7 +2,7 @@
 echo "Syncing with remote 'origin'"
 # Check if there are uncommited changes
 STASHED=false
-if git diff --name-only > /dev/null 2>&1; then # there are uncommited changes
+if ! git diff --name-only --exit-code > /dev/null 2>&1; then # there are uncommited changes
 	STASHED=true
 	read -p "There are uncommited changes. If you continue, these changes will be stashed and have to manually be pop-ed after syncing. Continue? (Y/n) " -n1 -r CONFIRMATION
 	echo
