c88f1c85dd7aa539348994a754d26ab2d3cbcdba
Lacrimus <lacrimus@maxi-dsl.de>
2020-10-17 14:59:49 +0200

    feat(initial commit of the carddeck page/components): added card/-deck component + [slug]-infopage

---
+++ src/components/common/Card.svelte
@@ -0,0 +1,19 @@
+<script>
+	/** Additional classes to pass in */
+	export let classes;
+</script>
+
+<!--
+    @component
+    Provides a resonsive multi-purpose card container with variable number of segments.
+-->
+
+<div class="overflow-hidden bg-gray-300 shadow-lg rounded-md p-1  {classes}">	
+	<!--Card header section-->
+	<slot name="header"/>
+
+	<!--Card body section--> 
+	<slot name="body"/>
+	<!--Card footer section-->
+	<slot name="footer"/>
+</div>
---
+++ src/routes/[slug]-infopage.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Carddeck from '../components/Carddeck.svelte';
+	import Nav from '../components/Nav.svelte';
+
+	export let title;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<Nav />
+
+<Carddeck title="Infoseite {title} | {process.globals.title}" />
