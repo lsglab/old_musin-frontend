ab5e06abed7624217f09122a9c39539777469f54
lsgadmin <lsgadmin@linux.fritz.box>
2020-10-23 17:34:06 +0200

    feat(_error.svelte): added error stack support

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -5,6 +5,8 @@
 
 	export let error;
 	export let status;
+
+	const stack = error.stack;
 </script>
 
 <style>
@@ -17,6 +19,10 @@
 	.card {
 		border-radius: 15px;
 	}
+
+	.stack {
+		@apply flex-wrap;
+	}
 </style>
 
 <svelte:head>
@@ -25,26 +31,32 @@
 
 <Section fullscreen="true">
 	<div
-		class="flex flex-wrap items-center justify-center p-5 m-auto md:p-10 lg:p-20 xl:p-20 lg:flex-no-wrap xl:flex-no-wrap">
-		<h1 class="z-20 text-white lg:-mt-48 h1">{status}</h1>
+		class="flex flex-wrap items-center justify-center p-5 m-auto md:p-10 lg:p-20 xl:p-20 lg:flex-no-wrap xl:flex-no-wrap"
+		class:stack>
+		<h1 class="z-20 text-white {!stack ? 'lg:-mt-48' : ''} h1">{status}</h1>
 
 		<div class="z-10 h-auto mt-10 bg-white shadow-2xl md:-ml-6 card">
 			<div class="m-5 ml-10">
 				<h2>{status === 404 ? 'Seite nicht gefunden' : error.message}</h2>
-				{#if error.stack || status === 404}
-					{#if status === 404}
-						<div class="my-6">
-							<p>
-								Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL aufgerufen haben -
-								bitte überprüfen Sie diese noch einmal. Oder aber wir haben die betreffende Seite
-								archiviert, verschoben oder umbennant.
-							</p>
-							<p>Vielleicht können Sie den von ihnen gewünscheten Inhalt über unsere Startseite finden</p>
-						</div>
-					{:else}
-						<pre>{error.stack}</pre>
+				<div class="my-6">
+					{#if error.stack || status === 404}
+						{#if status === 404}
+							<div>
+								<p>
+									Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL aufgerufen haben
+									- bitte überprüfen Sie diese noch einmal. Oder aber wir haben die betreffende Seite
+									archiviert, verschoben oder umbennant.
+								</p>
+								<p>
+									Vielleicht können Sie den von ihnen gewünscheten Inhalt über unsere Startseite
+									finden
+								</p>
+							</div>
+						{:else}
+							<pre>{error.stack}</pre>
+						{/if}
 					{/if}
-				{/if}
+				</div>
 				<div class="btn-wrap">
 					<Button buttonText="Zur Startseite" link="true" href="/" />
 				</div>
