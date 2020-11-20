c2bd5a0fcb303809157d9764d4e47b5147d9f6de
Lacrimus <lacrimus@maxi-dsl.de>
2020-11-20 17:00:13 +0100

    feat(artcollection.svelte): added image container

--- src/routes/artcollection.svelte
+++ src/routes/artcollection.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Flex from '../components/common/Flex.svelte';
+	import ShadowImage from '../components/common/ShadowImage.svelte';
 
 	export let title;
+	export let src = '../assets/media/lsg.png';
 </script>
 
 <style lang="scss">
@@ -14,5 +16,8 @@
 	<title>{title}</title>
 </svelte:head>
 <div id="artcollection">
-	<Flex />
+	<h4 align="center" class="mb-2">{title} | {process.globals.title}</h4>
+	<Flex wrap="true" justify="center" align="center" classes="px-2 gap-4">
+		<ShadowImage classes="bg-gray-300 shadow-lg rounded-md p-2" background="false" scr="../assets/media/lsg.png" />
+	</Flex>
 </div>
