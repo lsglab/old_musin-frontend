0c016eb233fc1718313e9a2a609e145e69af2acf
jamwag <james.wagner@outlook.de>
2020-11-29 00:31:22 +0100

    feat: improve the page design and split the main page into components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,16 @@
-﻿<style>
+﻿<script>
+	import Awards from '../components/Awards.svelte';
+	import Calender from '../components/Calender.svelte';
+</script>
+
+<style lang="scss">
 	* {
 		word-break: normal;
 	}
 
-	.awards::before {
-		content: '';
-		width: 100%;
-		height: 1px;
-		position: absolute;
-		top: -20px;
-		left: 0;
-		background: linear-gradient(
-			90deg,
-			rgba(66, 71, 112, 0.09),
-			rgba(66, 71, 112, 0.09) 50%,
-			transparent 0,
-			transparent
-		);
-		background-size: 8px 1px;
+	.school-img {
+		background-image: resolve('bg.jpg');
+		border-radius: 0.75rem;
 	}
 </style>
 
@@ -27,10 +20,10 @@
 
 <div class="" style="background: #f6f9fc;margin-top: -80px;">
 	<header
-		class="w-full flex flex-no-wrap justify-between"
-		style="min-height: calc(100vh - 90px); padding: 0 15vw; padding-top: 40px;">
-		<div class="flex w-auto items-center justify-center flex-col w-1/2 z-10">
-			<div class="m-12 mr-24 ml-0" style="max-width: 500px">
+		class="flex flex-no-wrap justify-between w-full"
+		style="min-height: 100vh; padding: 0 15vw; padding-top: 40px;">
+		<div class="z-10 flex flex-col items-center justify-center w-1/2">
+			<div class="m-12 ml-0 mr-24" style="max-width: 500px">
 				<h1 class="break-normal" style="font-size: 45px; line-height: 48px; color: #0a2540;">
 					Städtisches Louise Schroeder Gymnasium München
 				</h1>
@@ -44,179 +37,30 @@
 				<p style="margin-top: 40px; color: #778; font-size: 14px">Referenzschule der TU München</p>
 			</div>
 		</div>
-		<div class="w-full flex items-center justify-center flex-col z-10">
-			<div>
-				<img
-					class="w-full rounded-lg shadow-lg"
-					src="https://lsg.musin.de/homepage/images/header-images/schulhof_mini.jpg"
-					alt="" />
+		<div class="z-10 flex flex-col items-center justify-center w-full">
+			<div class="w-full" style="padding: 10vh 0;height:90vh;">
+				<img class="w-full h-full bg-cover shadow-lg school-img" alt="" />
 			</div>
 		</div>
 	</header>
-	<div
-		class="awards w-full flex h-auto flex-wrap justify-between relative"
-		id="logo-section"
-		style="padding: 0 15vw; padding-bottom: 30px;">
-		<img
-			class="logo"
-			style="height: 60px; filter: grayscale(1); mix-blend-mode: multiply;"
-			src="https://lsg.musin.de/homepage/images/LOGOsorsmc_SCREEN_80mm_RGB_mini.jpg"
-			alt="" />
-		<img
-			class="logo"
-			style="height: 60px; filter: grayscale(1); mix-blend-mode: multiply;"
-			src="https://lsg.musin.de/homepage/images/delf.jpg"
-			alt="" />
-		<img
-			class="logo"
-			style="height: 60px; filter: grayscale(1); mix-blend-mode: multiply;"
-			src="https://lsg.musin.de/homepage/images/cae.png"
-			alt="" />
-		<img
-			class="logo"
-			style="height: 60px; filter: grayscale(1); mix-blend-mode: multiply;"
-			src="https://lsg.musin.de/homepage/images/cils.png"
-			alt="" />
-		<img
-			class="logo"
-			style="height: 60px; filter: grayscale(1); mix-blend-mode: multiply;"
-			src="https://lsg.musin.de/homepage/images/KombiLogo.png"
-			alt="" />
-	</div>
+	<Awards />
 </div>
 <div
-	class="appointment-section w-full h-auto bg-white flex items-center flex-col"
+	class="flex flex-col items-center justify-center w-full h-full bg-white appointment-section"
 	style="min-height: 80vh; z-index: -1;"
 	id="termine">
-	<h1
-		class="text-center font-bold"
-		style="margin-top: 80px; font-size: 45px; color: #336; font-family: 'Open Sans', 'Segoe UI', sans-serif; z-index: 99;">
-		Aktuelle Termine
-	</h1>
-	<p
-		class="font-hairline text-center mt-0"
-		style="font-size: 24px; line-height: 40px; color: #447; font-family: 'Open Sans', 'Segoe UI', sans-serif; z-index: 999; padding: 0 20px;">
-		Alle demnächst anstehende Termine des Louise-Schroeder-Gymnasiums:
-	</p>
-	<div
-		class="appointments h-auto bg-white"
-		style="width: 80%; max-width: 1000px; box-shadow: 0 5px 15px rgba(0,0,0,0.2); border-radius: 10px; margin-top: 40px; z-index: 999;">
-		<ul class="w-full h-full list-none pl-0 m-0">
-			<li
-				class="w-full ml-0 pl-0 flex flex-no-wrap"
-				style="background: #fafbff; min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div class="time flex flex-col items-center justify-center" style="min-width: 100px; padding: 20px;">
-					<span class="font-normal" style="font-size: 16px; color: #667">Uhrzeit</span>
-				</div>
-				<div
-					class="date flex items-start font-medium"
-					style="min-width: 120px; font-size: 16px; color: #667; padding: 20px;">
-					Datum
-				</div>
-				<div
-					class="content flex align-center w-full justify-start font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					Inhalt
-				</div>
-			</li>
-			<li
-				class="ml-0 w-full flex flex-no-wrap pl-0"
-				style="min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div
-					class="time text-base flex flex-col items-center justify-center"
-					style="min-width: 100px; padding: 20px;">
-					<span style="font-size: 16px;">9:00</span>
-					<span style="font-size: 16px;">9:30</span>
-				</div>
-				<div
-					class="date text-base flex items-start font-medium"
-					style="font-size: 16px;color: #667; width: 130px; padding: 20px;">
-					13.08.2020
-				</div>
-				<div
-					class="content text-base flex align-center justify-start w-full font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					Einsichtnahme in die korrigierten schriftlichen Abituraufgaben unter Aufsicht
-				</div>
-			</li>
-			<li
-				class="w-full ml-0 pl-0 flex flex-no-wrap"
-				style="min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div
-					class="time flex flex-col items-center justify-center"
-					style="font-size: 16px; min-width: 100px; padding: 20px;">
-					<span style="font-size: 16px;">----</span>
-				</div>
-				<div
-					class="date text-base flex items-start font-medium"
-					style="font-size: 16px;color: #667; width: 130px; padding: 20px;">
-					24.07.2020
-				</div>
-				<div
-					class="content text-base flex align-center justify-start w-full font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					Q11/2: Vollversammlung: mit Zeugnisausgabe (ausgenommen Ersatzprüflinge)
-				</div>
-			</li>
-			<li
-				class="w-full ml-0 pl-0 flex flex-no-wrap"
-				style="min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div
-					class="time flex flex-col items-center justify-center"
-					style="font-size: 16px; min-width: 100px; padding: 20px;">
-					<span style="font-size: 16px;">----</span>
-				</div>
-				<div
-					class="date text-base flex items-start font-medium"
-					style="font-size: 16px;color: #667; width: 130px; padding: 20px;">
-					24.07.2020
-				</div>
-				<div
-					class="content text-base flex align-center justify-start w-full font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					05-10: Ausgabe der Jahreszeugnisse (ausgenommen Ersatzprüflinge)
-				</div>
-			</li>
-			<li
-				class="w-full ml-0 pl-0 flex flex-no-wrap"
-				style="min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div
-					class="time flex flex-col items-center justify-center"
-					style="font-size: 16px; min-width: 100px; padding: 20px;">
-					<span style="font-size: 16px;">10:00</span>
-				</div>
-				<div
-					class="date text-base flex items-start font-medium"
-					style="font-size: 16px;color: #667; width: 130px; padding: 20px;">
-					24.07.2020
-				</div>
-				<div
-					class="content text-base flex align-center justify-start w-full font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					Termin für die schriftliche Abmeldung von der Schule bei Schulwechsel oder Übertritt ins Berufsleben
-				</div>
-			</li>
-			<li
-				class="w-full ml-0 pl-0 flex flex-no-wrap"
-				style="min-height: 50px; border-bottom: 1px solid rgba(0,0,0,0.1);">
-				<div
-					class="time flex flex-col items-center justify-center"
-					style="font-size: 16px; min-width: 100px; padding: 20px;">
-					<span style="font-size: 16px;">10:00</span>
-				</div>
-				<div
-					class="date text-base flex items-start font-medium"
-					style="font-size: 16px;color: #667; width: 130px; padding: 20px;">
-					24.07.2020
-				</div>
-				<div
-					class="content text-base flex align-center justify-start w-full font-medium"
-					style="font-size: 16px;color: #667; padding: 20px;">
-					Termin für die schriftliche Abmeldung vom Religionsunterricht, um am Ethikunterricht teilnehmen zu
-					können.
-				</div>
-			</li>
-		</ul>
+	<div class="flex flex-col items-center w-full">
+		<h1
+			class="font-bold text-center"
+			style="font-size: 45px; color: #336; font-family: 'Open Sans', 'Segoe UI', sans-serif; z-index: 99;">
+			Aktuelle Termine
+		</h1>
+		<p
+			class="mt-0 font-hairline text-center"
+			style="font-size: 24px; line-height: 40px; color: #447; font-family: 'Open Sans', 'Segoe UI', sans-serif; z-index: 999; padding: 0 20px;margin-top: 10px;">
+			Alle demnächst anstehende Termine des Louise-Schroeder-Gymnasiums:
+		</p>
+		<Calender />
+		<a href="/termine" style="margin-top: 10px;font-size: 18px;">Zu allen Terminen</a>
 	</div>
-	<a href="/termine" style="margin-top: 30px;font-size: 18px;margin-bottom: 50px;">Zu allen Terminen</a>
 </div>
