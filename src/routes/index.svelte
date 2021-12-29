fd87d50c1226f7ad84d408aee4d162522b7b8e8b
Test <test@gmail.com>
2021-12-29 10:44:41 +0100

    feat: Test deleted site: /termine

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,43 +1,42 @@
-
 <script context="module">
-    import Export from '../components/cms/export.svelte';
-    import request from '../Utils/requests';
+	import Export from '../components/cms/export.svelte';
+	import request from '../Utils/requests';
 
-    async function fetchCustomComponents(apiUrl) {
-        const res = await request(`${apiUrl}/components?_norelations=true`, 'get', {}, false);
+	async function fetchCustomComponents(apiUrl) {
+		const res = await request(`${apiUrl}/components?_norelations=true`, 'get', {}, false);
 
-        if (res.status === 200) {
-            return res.data.components;
-        }
-        return [];
-    }
+		if (res.status === 200) {
+			return res.data.components;
+		}
+		return [];
+	}
 
-    async function fetchData(apiUrl, path) {
-        const res = await request(`${apiUrl}/sites?path=${path}`, 'get', {}, false);
+	async function fetchData(apiUrl, path) {
+		const res = await request(`${apiUrl}/sites?path=${path}`, 'get', {}, false);
 
-        return JSON.parse(res.data.sites[0].blueprint);
-    }
+		return JSON.parse(res.data.sites[0].blueprint);
+	}
 
-    export async function preload(page, session) {
-        const apiUrl = session.globals.apiUrl;
-        let path = page.path;
+	export async function preload(page, session) {
+		const apiUrl = session.globals.apiUrl;
+		let path = page.path;
 
-        if(path === '/'){
-            path = '/index';
-        }
+		if (path === '/') {
+			path = '/index';
+		}
 
-        const customComponents = await fetchCustomComponents(apiUrl);
-        const data = await fetchData(apiUrl, path);
+		const customComponents = await fetchCustomComponents(apiUrl);
+		const data = await fetchData(apiUrl, path);
 
-        return { customComponents, data };
-    }
+		return { customComponents, data };
+	}
 </script>
 
 <script>
-    export let customComponents;
-    export let data;
+	export let customComponents;
+	export let data;
 </script>
 
-<div style="visibility:hidden;"><a href='/termine' alt=''>'/termine'</a></div>
+<div style="visibility:hidden;"><a href="/termine" alt="">'/termine'</a></div>
 
 <Export data="{data}" customComponents="{customComponents}" />
