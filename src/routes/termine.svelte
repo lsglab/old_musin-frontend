aec3ce116103017c3748314d851155906b9525dd
Simon Weckler <simon.weckler@mnet-online.de>
2021-12-29 12:37:38 +0100

    feat: Simon Weckler created site: /termine

--- src/routes/termine.svelte
+++ src/routes/termine.svelte
@@ -37,7 +37,7 @@
     export let data;
 </script>
 
-<div style="visibility:hidden;"><a href='/termine' alt=''>'/termine'</a></div>
+
 
 <Export data="{data}" customComponents="{customComponents}" />
 
