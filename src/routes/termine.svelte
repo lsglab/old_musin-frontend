67f5a34432e25bca8b705971d05c71e80b7cf4cc
Test <test@gmail.com>
2021-12-29 10:40:13 +0100

    feat: Test created site: /termine

--- src/routes/termine.svelte
+++ src/routes/termine.svelte
@@ -38,6 +38,6 @@
     export let data;
 </script>
 
-<div style="visibility:hidden;"><a href='/termine' alt=''>'/termine'</a></div>
+
 
 <Export data="{data}" customComponents="{customComponents}" />
