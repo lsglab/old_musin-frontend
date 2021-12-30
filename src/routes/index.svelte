662badec8438b4aa359ed7e4bcb6c8291f82330c
Simon Weckler <simon.weckler@mnet-online.de>
2021-12-30 10:48:37 +0100

    feat: Simon Weckler deleted site: /termine

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,47 @@
 <script context="module">
-    import { request } from '../Utils/requests';
-    import Export from '../components/cms/export.svelte';
+	import { request } from '../Utils/requests';
+	import Export from '../components/cms/export.svelte';
 
-    async function fetchCustomComponents(apiUrl) {
-        const res = await request(`${apiUrl}/components?_norelations=true`, 'get', {}, false);
+	async function fetchCustomComponents(apiUrl) {
+		const res = await request(`${apiUrl}/components?_norelations=true`, 'get', {}, false);
 
-        if (res.status === 200) {
-            return res.data.components;
-        }
-        return [];
-    }
+		if (res.status === 200) {
+			return res.data.components;
+		}
+		return [];
+	}
 
-    async function fetchData(apiUrl, path, context) {
-        const res = await request(`${apiUrl}/sites?path=${path}`, 'get', {}, false);
+	async function fetchData(apiUrl, path, context) {
+		const res = await request(`${apiUrl}/sites?path=${path}`, 'get', {}, false);
 
 		if (res.status !== 200) {
-			// eslint-disable-next-line babel/no-invalid-this
 			return context.error(res.status, res.data.message);
 		}
 
-        return JSON.parse(res.data.sites[0].blueprint);
-    }
+		return JSON.parse(res.data.sites[0].blueprint);
+	}
 
-    export async function preload(page, session) {
-        const apiUrl = session.globals.apiUrl;
-        let path = page.path;
+	export async function preload(page, session) {
+		const apiUrl = session.globals.apiUrl;
+		let path = page.path;
 
-        if(path === '/'){
-            path = '/index';
-        }
+		if (path === '/') {
+			path = '/index';
+		}
 
-        const customComponents = await fetchCustomComponents(apiUrl);
-        // eslint-disable-next-line babel/no-invalid-this
+		const customComponents = await fetchCustomComponents(apiUrl);
+		// eslint-disable-next-line babel/no-invalid-this
 		const data = await fetchData(apiUrl, path, this);
 
-        return { customComponents, data };
-    }
+		return { customComponents, data };
+	}
 </script>
 
 <script>
-    export let customComponents;
-    export let data;
+	export let customComponents;
+	export let data;
 </script>
 
-<div style="visibility:hidden;"><a href='/termine' alt=''>'/termine'</a></div>
+<div style="visibility:hidden;"><a href="/termine" alt="">'/termine'</a></div>
 
 <Export data="{data}" customComponents="{customComponents}" />
-
